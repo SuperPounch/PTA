010a890bd5ef23fb4d8af9bf2bc4a244ba7b2168
landlord <1240847431@qq.com>
2020-02-21 20:10:10 +0800

    1002查看Github上内容

--- PTA-B/src/B1002.java
+++ PTA-B/src/B1002.java
@@ -9,22 +9,56 @@ import java.util.Scanner;
 public class B1002 {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        if(in.hasNext()){
-            String a =String.valueOf(in.toString()) ;
-            System.out.println("a = " + a);
+        String str = new Scanner(System.in).nextLine();
+        char[] chars = str.toCharArray();
+        int sum = 0;
+        for (int i = 0; i < chars.length; i++) {
+            sum += chars[i] - '0';
         }
-       /* int res= 0;
-        char[] chars=String.valueOf(a).toCharArray();
-        for(int j=0;j<chars.length;j++){
-          // res+=Long.valueOf(chars[j]);
-           res+=Integer.parseInt(String.valueOf(chars[j]));
+        char[] c = (sum + "").toCharArray();
+        boolean flag = false;
+        for (int i = 0; i < c.length; ++i) {
+            if (flag) {
+                System.out.print(" ");
+            }
+            out(c[i]);
+            flag = true;
         }
-        System.out.println(res);*/
     }
 
-   /* private static String soulution(Long i) {
-        Long
-        return null;
-    }*/
+    private static void out(char c) {
+        switch (c) {
+            case '1':
+                System.out.print("yi");
+                break;
+            case '2':
+                System.out.print("er");
+                break;
+            case '3':
+                System.out.print("san");
+                break;
+            case '4':
+                System.out.print("si");
+                break;
+            case '5':
+                System.out.print("wu");
+                break;
+            case '6':
+                System.out.print("liu");
+                break;
+            case '7':
+                System.out.print("qi");
+                break;
+            case '8':
+                System.out.print("ba");
+                break;
+            case '9':
+                System.out.print("jiu");
+                break;
+            case '0':
+                System.out.print("ling");
+                break;
+
+        }
+    }
 }
